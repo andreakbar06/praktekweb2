858a0288983270c99f7e2a1d1a6d1bf46234fd46
andre <m.andreakbar@gmail.com>
2017-05-24 09:59:39 +0700

    update tanggal untuk tampilan depan

--- pages/depan.php
+++ pages/depan.php
@@ -6,7 +6,7 @@
                 <p class="lead">
                     by <a href="index.php">Start Bootstrap</a>
                 </p>
-                <p><i class="fa fa-clock-o"></i> <?php echo $dt['tanggal'] ?></p>
+                <p><i class="fa fa-clock-o"></i> <?php echo date('d-F-Y',strtotime($dt['tanggal'])); ?></p>
                 <hr>
                 <a href="blog-post.html">
                 </a>
